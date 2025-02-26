db12c18bbc9e1ba0d73ba93d7fa97e96b3691463
Hans Pagel <hanspagel@users.noreply.github.com>
2025-02-26 10:44:08 +0100

    Update packages/api-client/src/views/Request/RequestSection/RequestTable.vue

--- packages/api-client/src/views/Request/RequestSection/RequestTable.vue
+++ packages/api-client/src/views/Request/RequestSection/RequestTable.vue
@@ -12,7 +12,7 @@ import DataTableCheckbox from '@/components/DataTable/DataTableCheckbox.vue'
 import DataTableRow from '@/components/DataTable/DataTableRow.vue'
 import type { EnvVariable } from '@/store/active-entities'
 
-import { hasItemProperties, parameterIsInvalid } from '../libs/request'
+import { hasItemProperties, parameterIsInvalid } from '@/libs/request'
 import RequestTableTooltip from './RequestTableTooltip.vue'
 
 const props = withDefaults(
